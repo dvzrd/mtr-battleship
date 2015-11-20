3b3a243beb678a97d0dbc58cb0d274ec399accbd
Damir <damir@withpulp.com>
2015-11-20 17:10:30 -0500

    updates to games collection, added allow/deny for users

--- app/collections/games.js
+++ app/collections/games.js
@@ -1,1 +1,26 @@
 Games = new Mongo.Collection('games');
+
+Games.allow({
+        insert: () => false,
+    update: () => false,
+    remove: () => false
+});
+
+Games.deny({
+        insert: () => true,
+    update: () => true,
+    remove: () => true
+});
+
+let GamesSchema = new SimpleSchema({
+    'name': {
+        type: String,
+        label: 'The name of the game.'
+    },
+    'creator': {
+        type: String,
+        label: 'The user that created the game'
+    }
+});
+
+Games.attachSchema( GamesSchema );
