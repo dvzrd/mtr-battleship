d08db67377a57789bb7cef7d37a4dd5b0577cf6a
Damir <damir@withpulp.com>
2015-11-22 05:12:18 -0500

    style updates

--- app/client/components/modules/common/menu.jsx
+++ app/client/components/modules/common/menu.jsx
@@ -1,8 +1,29 @@
 App.Menu = React.createClass({
+    leftButtons() {
+        if (!Meteor.loggingIn() && Meteor.user()) {
+            return (
+                <a className="secondary icon button" href="#" onClick={Meteor.logout}>
+                    <i className="fa fa-power-off"></i>
+                </a>
+            );
+        } else {
+            return (
+                <a className="secondary icon button" href={RouterHelpers.pathFor('login')}>
+                    <i className="fa fa-plug"></i>
+                </a>
+            );
+        }
+    },
     render() {
         return (
             <module className="menu module">
-                menu module
+                <div className="left buttons">
+                    {this.leftButtons()}
+                </div>
+                <div className="right buttons">
+                    <button type="button" className="secondary icon button"><i className="fa fa-bars"></i></button>
+
+                </div>
             </module>
         );
     }
