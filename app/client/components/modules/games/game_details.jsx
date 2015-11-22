9e36c861a376416ffbc6b5b893caca27cd0340e6
Damir <damir@withpulp.com>
2015-11-22 06:19:26 -0500

    more updates to game board and game details

---
+++ app/client/components/modules/games/game_details.jsx
@@ -0,0 +1,38 @@
+App.GameDetails = React.createClass({
+    mixins: [ReactMeteorData],
+    PropTypes: {},
+
+    shouldComponentUpdate() {
+        return true;
+    },
+
+    getMeteorData() {
+        let subscription = Meteor.subscribe('games');
+
+        return {
+            isLoading: !subscription.ready(),
+            games: Games.find().fetch()
+        };
+    },
+
+    render() {
+        if (this.data.isLoading) {
+            return <App.Loading />;
+        } else {
+            return (
+                <module className="game details module">
+                    <h2 className="title">Title {this.data.games.title}</h2>
+
+                    <div className="players">
+                        <h4 className="left player">
+                            Player 1 <small className="points">10/50 points</small>
+                        </h4>
+                        <h4 className="right player">
+                            Player 2 <small className="points">10/50 points</small>
+                        </h4>
+                    </div>
+                </module>
+            );
+        }
+    }
+});
--- app/client/components/views/private/battle.jsx
+++ app/client/components/views/private/battle.jsx
@@ -2,6 +2,7 @@ App.Battle = React.createClass({
     render() {
         return (
             <view className="battle view">
+                <App.GameDetails />
                 <App.GameBoard />
             </view>
         );
