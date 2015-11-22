deb875b22894a5f6bf1a15a0cfdd4b5c9beabd24
Damir <damir@withpulp.com>
2015-11-21 19:55:22 -0500

    smoke updates to common modules

--- app/client/components/modules/common/copyright.jsx
+++ app/client/components/modules/common/copyright.jsx
@@ -1,8 +1,11 @@
 App.Copyright = React.createClass({
     render() {
         return (
-            <module className="copyright">
-                <p>copyright</p>
+            <module className="copyright module">
+                <p className="copyright">&copy;2015, <a className="creator" href="http://damirvazgird.com"
+                                                        target="_blank">Damir Vazgird</a>. Code available on <a
+                    className="creator" href="https://github.com/amazingBastard/mtr-battleship"
+                    target="_blank">Github</a>.</p>
             </module>
         );
     }
--- app/client/components/modules/common/logo.jsx
+++ app/client/components/modules/common/logo.jsx
@@ -1,8 +1,8 @@
 App.Logo = React.createClass({
     render() {
         return (
-            <module className="logo">
-                <h1 className="title">Space Battleship</h1>
+            <module className="logo module">
+                <h1 className="logo">Space Battleship</h1>
             </module>
         );
     }
