a645d32c844a2ada467062257848e482faa6f05e
Damir <damir@withpulp.com>
2015-11-22 15:17:06 -0500

    added ids to gameboard

--- app/client/components/modules/games/game_board.jsx
+++ app/client/components/modules/games/game_board.jsx
@@ -21,40 +21,40 @@ App.GameBoard = React.createClass({
         } else {
             return (
                 <module className="game board module">
-                    <div className="grid">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
+                    <div className="grid" id="A">
+                        <div className="cell" id="1A"></div>
+                        <div className="cell" id="2A"></div>
+                        <div className="cell" id="3A"></div>
+                        <div className="cell" id="4A"></div>
+                        <div className="cell" id="5A"></div>
                     </div>
-                    <div className="grid">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
+                    <div className="grid" id="B">
+                        <div className="cell" id="1B"></div>
+                        <div className="cell" id="2B"></div>
+                        <div className="cell" id="3B"></div>
+                        <div className="cell" id="4B"></div>
+                        <div className="cell" id="5B"></div>
                     </div>
-                    <div className="grid">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
+                    <div className="grid" id="C">
+                        <div className="cell" id="1C"></div>
+                        <div className="cell" id="2C"></div>
+                        <div className="cell" id="3C"></div>
+                        <div className="cell" id="4C"></div>
+                        <div className="cell" id="5C"></div>
                     </div>
-                    <div className="grid">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
+                    <div className="grid" id="D">
+                        <div className="cell" id="1D"></div>
+                        <div className="cell" id="2D"></div>
+                        <div className="cell" id="3D"></div>
+                        <div className="cell" id="4D"></div>
+                        <div className="cell" id="5D"></div>
                     </div>
-                    <div className="grid">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
+                    <div className="grid" id="E">
+                        <div className="cell" id="1E"></div>
+                        <div className="cell" id="2E"></div>
+                        <div className="cell" id="3E"></div>
+                        <div className="cell" id="4E"></div>
+                        <div className="cell" id="5E"></div>
                     </div>
                 </module>
             );
