1ab4dba4448bf2ff08b622de342251f9191257e6
Damir <damir@withpulp.com>
2015-11-22 01:30:13 -0500

    styling games list

--- app/client/components/modules/games/game.jsx
+++ app/client/components/modules/games/game.jsx
@@ -2,7 +2,12 @@ App.Game = React.createClass({
     render() {
         return (
             <li className="game item">
-                <h2 className="title">{this.props.game.title}</h2>
+                <a className="route link" href="#">
+                    <h2 className="title">
+                        {this.props.game.title} <small className="date">Render date here</small>
+                    </h2>
+                    <span className="creator">Created by {this.props.game.creator}</span>
+                </a>
             </li>
         );
     }
