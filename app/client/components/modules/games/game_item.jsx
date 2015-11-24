6fdaf7a0859e5b5c8a4daceabf91cffb8cc8f28d
Damir <damir@withpulp.com>
2015-11-24 04:19:29 -0500

    handle game join logic

--- app/client/components/modules/games/game_item.jsx
+++ app/client/components/modules/games/game_item.jsx
@@ -2,30 +2,37 @@ App.GameItem = React.createClass({
     handleGameJoin(event) {
         event.preventDefault();
 
+        // @TODO: Move this to joinGame method (and Modules.client)
+
         var pathDef = '/battle/:_id',
             params = {_id: this.props.game._id},
             path = FlowRouter.path(pathDef, params),
-            user = Meteor.user();
+            currentUser = Meteor.user();
 
-        FlowRouter.go(path);
-
-        //if (user) {
-        //    console.log(user + ' is creator, just route');
-        //    FlowRouter.go(path);
-        //} else {
-        //    console.log('not the creator, but check if destroyer exists');
-        //}
+        if (this.props.game.creator === currentUser.username) {
+            FlowRouter.go(path);
+        }
+        if (this.props.game.destroyer === currentUser.username) {
+            FlowRouter.go(path);
+        }
+        if (this.props.game.playerCount === 2) {
+            Bert.alert('This game is full', 'warning');
+            // @TODO: method for joining a recently created and available game
+        } else {
+            Bert.alert('Making game', 'warning');
+        }
     },
     render() {
         return (
-            <li className="game item">
-                <div className="route link" onClick={this.handleGameJoin}>
-                    <h2 className="title">
-                        {this.props.game.title} <small className="date">Render date here</small>
-                    </h2>
-                    <span className="creator">Created by {this.props.game.creator}</span>
-                    <span className="count">Players: {this.props.game.playerCount}</span>
-                </div>
+            <li className="game item" onClick={this.handleGameJoin}>
+
+                <h2 className="title">
+                    {this.props.game.title}
+                    <small className="date">Render date here</small>
+                </h2>
+                <span className="creator">Created by {this.props.game.creator}</span>
+                <span className="count">Players: {this.props.game.playerCount}</span>
+
             </li>
         );
     }
