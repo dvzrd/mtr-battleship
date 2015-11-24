55c92cfc10b951cc76a8c2d38527444f8e782926
Damir <damir@withpulp.com>
2015-11-24 08:57:19 -0500

    updated join game method and component

--- app/client/components/modules/games/game_item.jsx
+++ app/client/components/modules/games/game_item.jsx
@@ -2,37 +2,23 @@ App.GameItem = React.createClass({
     handleGameJoin(event) {
         event.preventDefault();
 
-        // @TODO: Move this to joinGame method (and Modules.client)
-
         var pathDef = '/battle/:_id',
             gameId = this.props.game._id,
             params = {_id: gameId},
             path = FlowRouter.path(pathDef, params),
             currentUser = Meteor.user();
 
-        if (currentUser === null) {
-            Bert.alert('You need to login first', 'warning');
-        }
-        if (this.props.game.creator === currentUser.username) {
-            FlowRouter.go(path);
-        }
-        if (this.props.game.destroyer === currentUser.username) {
-            FlowRouter.go(path);
+        if (!currentUser) {
+            Bert.alert('You need to be logged in to join a game', 'warning');
         } else {
-            // @TODO: fix this horrible logic - move into method
-            if (this.props.game.playerCount === 2 || this.props.game.creator === currentUser.username) {
-                // @TODO: method for joining a recently created and available game
-                Bert.alert('I cannot let you do that', 'warning');
-            } else {
-                Meteor.call('joinGame', gameId, (error) => {
-                    if (error) {
-                        Bert.alert(error.reason, 'warning');
-                    } else {
-                        Bert.alert('Get ready to destroy!', 'success');
-                        FlowRouter.go(path);
-                    }
-                });
-            }
+            Meteor.call('joinGame', gameId, (error) => {
+                if (error) {
+                    Bert.alert(error.reason, 'warning');
+                } else {
+                    Bert.alert('Get ready to destroy!', 'success');
+                    FlowRouter.go(path);
+                }
+            });
         }
     },
     render() {
--- app/lib/methods/games.js
+++ app/lib/methods/games.js
@@ -14,15 +14,31 @@ Meteor.methods({
     joinGame(gameId) {
         check(gameId, String);
 
-        var user = Meteor.user();
+        var user = Meteor.user(),
+            game = Games.findOne({_id: gameId}),
+            userIsPlaying = game.creator === user.username || game.destroyer === user.username,
+            gameIsFull = game.playerCount === 2 && !userIsPlaying,
+            pathDef = '/battle/:_id',
+            params = {_id: gameId},
+            path = FlowRouter.path(pathDef, params);
 
-        try {
-            var game = Games.update(gameId, {
-                $set: {'destroyer': user.username, 'playerCount': 2}
-            });
-            return game;
-        } catch (exception) {
-            return exception;
+        if (!user) {
+            throw new Meteor.Error('user-not-logged-in', 'Need to be logged in to join a game');
+        }
+        if (!game) {
+            throw new Meteor.Error('game-does-not-exist', 'This game is not in the collection');
+        }
+        if (gameIsFull) {
+            // @TODO: method for joining a recently created and available game
+            return Meteor.Error('game-full', 'This game is full.');
+        } else {
+            if (userIsPlaying) {
+                FlowRouter.go(path);
+            } else {
+                Games.update(gameId, {
+                    $set: {'destroyer': user.username, 'playerCount': 2}
+                });
+            }
         }
     },
     completeGame(gameId) {
