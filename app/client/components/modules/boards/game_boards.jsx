726daccfd835c5bfde7867bb65a1ecaf28022f30
Damir <damir@withpulp.com>
2015-11-24 20:44:10 -0500

    game boards wip for deta handle

--- app/client/components/modules/boards/game_boards.jsx
+++ app/client/components/modules/boards/game_boards.jsx
@@ -1,11 +1,30 @@
 App.GameBoards = React.createClass({
+    mixins: [ReactMeteorData],
+    getMeteorData() {
+        let selector = {gameId: '', owner: ''};
+        let subscription = Meteor.subscribe('boards', selector);
+
+        return {
+            isLoading: !subscription.ready(),
+            boards: Boards.find({}, {sort: {createdAt: -1}}).fetch()
+        };
+    },
+    renderGameBoard() {
+
+    },
     render () {
-        return (
-            <module className="game boards module">
-                <App.GameBoard />
-                <App.GameDetails />
-                <App.GameBoard />
-            </module>
-        )
+        let unchallenged = this.data.boards.length === 1;
+
+        if (this.data.isLoading) {
+            return <App.Loading />;
+        } else {
+            return (
+                <module className="game boards module">
+                    <App.GameBoard />
+                    <App.GameDetails />
+                    <App.GameBoard />
+                </module>
+            );
+        }
     }
 });
