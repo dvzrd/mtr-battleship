24926b2b6c3411442db1ff9c6bbbcc707d2c15de
Damir <damir@withpulp.com>
2015-11-24 22:15:10 -0500

    game boards wip

--- app/client/components/modules/boards/game_boards.jsx
+++ app/client/components/modules/boards/game_boards.jsx
@@ -1,28 +1,39 @@
 App.GameBoards = React.createClass({
     mixins: [ReactMeteorData],
+    PropTypes: {},
+
+    shouldComponentUpdate() {
+        return true;
+    },
     getMeteorData() {
-        let selector = {gameId: '', owner: ''};
-        let subscription = Meteor.subscribe('boards', selector);
+        let gameId = FlowRouter.getParam('_id'),
+            selector = {gameId: gameId},
+            subscription = Meteor.subscribe('boards', selector);
 
         return {
             isLoading: !subscription.ready(),
             boards: Boards.find({}, {sort: {createdAt: -1}}).fetch()
         };
     },
-    renderGameBoard() {
-
+    renderGameCreatorBoard() {
+        return (
+            <App.GameBoard />
+        );
+    },
+    renderGameDestroyerBoard() {
+        return (
+            <App.GameBoard />
+        );
     },
     render () {
-        let unchallenged = this.data.boards.length === 1;
-
         if (this.data.isLoading) {
             return <App.Loading />;
         } else {
             return (
                 <module className="game boards module">
-                    <App.GameBoard />
+                    {this.renderGameCreatorBoard()}
                     <App.GameDetails />
-                    <App.GameBoard />
+                    {this.renderGameDestroyerBoard()}
                 </module>
             );
         }
--- app/server/publications/boards.js
+++ app/server/publications/boards.js
@@ -1,6 +1,6 @@
 Meteor.publish('boards', function (selector) {
     check(selector, Object);
-    return Boards.find(selector, {fields: {'owner': 1, 'gameId': 1}});
+    return Boards.find(selector, {fields: {'gameId': 1}});
 });
 
 Meteor.publish('board', function (boardId) {
