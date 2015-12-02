5e56aa370873f0a87c9bb41cb966eb79c200bfa7
Damir <damir@withpulp.com>
2015-12-02 12:46:42 -0500

    comments working

--- app/server/publications/comments.js
+++ app/server/publications/comments.js
@@ -2,3 +2,8 @@ Meteor.publish('comments', function (selector) {
     check(selector, Object);
     return Comments.find(selector);
 });
+
+Meteor.publish('comment', function (commentId) {
+    check(commentId, String);
+    return Games.find({_id: commentId});
+});
