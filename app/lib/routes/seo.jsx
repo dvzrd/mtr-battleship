8552288a4f48dc4ad6b235d273f2619db7299b86
Damir <damir@withpulp.com>
2015-12-02 15:21:09 -0500

    seo config

---
+++ app/lib/routes/seo.jsx
@@ -0,0 +1,12 @@
+SEO = new FlowRouterSEO();
+
+SEO.setDefaults({
+    title: 'Space Battle | A battleship game built with Meteor and React',
+    description: 'Battleship with a space theme - Space Battle is a Meteor and React app that lets you play with friends or against a bot.',
+    meta: {
+        'property="og:type"': 'webApp',
+        'property="og:site_name"': 'spacebattle.co',
+        'name="twitter:card"': 'Space Battle',
+        'name="twitter:site"': '@dvazgird'
+    }
+});
