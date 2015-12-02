7517464a05d4165e82177c064b9333c51778b311
Damir <damir@withpulp.com>
2015-12-02 11:01:49 -0500

    more style updates

--- app/client/components/modules/games/game_item.jsx
+++ app/client/components/modules/games/game_item.jsx
@@ -4,47 +4,52 @@ App.GameItem = React.createClass({
 
         // @TODO: refactor - break down these calls
         let user = Meteor.user(),
-            gameId = this.props.game._id,
-            userIsPlaying = this.props.game.creator === user.username || this.props.game.destroyer === user.username,
-            gameIsFull = this.props.game.playerCount === 2 && !userIsPlaying,
-            pathDef = '/battle/:_id',
-            params = {_id: gameId},
-            path = FlowRouter.path(pathDef, params);
+            gameId = this.props.game._id;
 
-        if (gameIsFull) {
-            // @TODO: method for joining a recently created and available game
-            Bert.alert('game-full', 'This game is full.', 'warning');
+        if (!user) {
+            Bert.alert('not-logged-in', 'You need to be logged in to join a game.', 'warning');
         } else {
-            if (userIsPlaying) {
-                FlowRouter.go(path);
+            userIsPlaying = this.props.game.creator === user.username || this.props.game.destroyer === user.username,
+                gameIsFull = this.props.game.playerCount === 2 && !userIsPlaying,
+                pathDef = '/battle/:_id',
+                params = {_id: gameId},
+                path = FlowRouter.path(pathDef, params);
+
+            if (gameIsFull) {
+                // @TODO: method for joining a recently created and available game
+                Bert.alert('game-full', 'This game is full.', 'warning');
             } else {
+                if (userIsPlaying) {
+                    FlowRouter.go(path);
+                } else {
 
-                let joinAttributes = {
-                    gameId: gameId,
-                    destroyer: user.username
-                };
+                    let joinAttributes = {
+                        gameId: gameId,
+                        destroyer: user.username
+                    };
 
-                Meteor.call('joinGame', joinAttributes, (error) => {
-                    if (error) {
-                        Bert.alert(error.reason, 'warning');
-                    } else {
-                        Bert.alert('Get ready to destroy!', 'success');
+                    Meteor.call('joinGame', joinAttributes, (error) => {
+                        if (error) {
+                            Bert.alert(error.reason, 'warning');
+                        } else {
+                            Bert.alert('Get ready to destroy!', 'success');
 
-                        // @TODO: refactor - separate function for createGameBoard
-                        let boardAttributes = {
-                            gameId: gameId,
-                            owner: user.username
-                        };
+                            // @TODO: refactor - separate function for createGameBoard
+                            let boardAttributes = {
+                                gameId: gameId,
+                                owner: user.username
+                            };
 
-                        Meteor.call('createGameBoard', boardAttributes, (error) => {
-                            if (error) {
-                                Bert.alert(error.reason, 'success');
-                            } else {
-                                FlowRouter.go(path);
-                            }
-                        });
-                    }
-                });
+                            Meteor.call('createGameBoard', boardAttributes, (error) => {
+                                if (error) {
+                                    Bert.alert(error.reason, 'success');
+                                } else {
+                                    FlowRouter.go(path);
+                                }
+                            });
+                        }
+                    });
+                }
             }
         }
     },
